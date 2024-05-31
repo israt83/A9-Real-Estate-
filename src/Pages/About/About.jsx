a45093a15960c6fd86481cd5139e53871c2f9444
israt83 <isratjahanfci@gmail.com>
2024-05-31 10:58:32 +0600

    create  a readme.md file

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,27 +1,27 @@
 
 
-// import { useEffect } from "react";
-// import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-// const About = () => {
-//     const location = useLocation();
+const About = () => {
+    const location = useLocation();
 
-//     useEffect(() => {
-//         document.title = 'REALESTATE/ABOUT'; 
-//     }, [location.pathname]); 
+    useEffect(() => {
+        document.title = 'REALESTATE/ABOUT'; 
+    }, [location.pathname]); 
 
-//     return (
-//         <>
-//             <div className="mt-32 border border-blue-900 text-center mx-auto w-full max-w-screen-lg h-auto lg:h-[300px] flex flex-col justify-center items-center my-10 p-4 lg:p-0" data-aos="zoom-in"
-//                 data-aos-easing="ease-out-cubic"
-//                 data-aos-duration='1000'>
-//                 <h1 className="text-2xl lg:text-4xl font-bold my-4 lg:my-7">About us</h1>
-//                 <p className="border border-dashed w-full lg:w-[500px] p-4 text-center text-[#706F6F] text-sm lg:text-base font-semibold">
-//                     You will find the latest information about us on this page. Our site is constantly evolving and growing. We provide a wide range of services. Our mission is to provide the best solution that helps everyone. If you want to contact us, please login to the login page and fill out the contact form on our contact section...
-//                 </p>
-//             </div>
-//         </>
-//     );
-// };
+    return (
+        <>
+            <div className="mt-32 border border-blue-900 text-center mx-auto w-full max-w-screen-lg h-auto lg:h-[300px] flex flex-col justify-center items-center my-10 p-4 lg:p-0" data-aos="zoom-in"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration='1000'>
+                <h1 className="text-2xl lg:text-4xl font-bold my-4 lg:my-7">About us</h1>
+                <p className="border border-dashed w-full lg:w-[500px] p-4 text-center text-[#706F6F] text-sm lg:text-base font-semibold">
+                    You will find the latest information about us on this page. Our site is constantly evolving and growing. We provide a wide range of services. Our mission is to provide the best solution that helps everyone. If you want to contact us, please login to the login page and fill out the contact form on our contact section...
+                </p>
+            </div>
+        </>
+    );
+};
 
-// export default About;
+export default About;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,103 +1,103 @@
 
 
-// import { Link, useLocation, useNavigate } from 'react-router-dom';
-// import SocialLogin from './SocialLogin';
-// import { useForm } from 'react-hook-form';
-// import useAuth from '../../hooks/useAuth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import SocialLogin from './SocialLogin';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
 
-// import { useEffect, useState } from 'react';
-// import { toast, ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import { GoEyeClosed } from "react-icons/go";
-// import { IoEyeOutline } from "react-icons/io5";
+import { useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { GoEyeClosed } from "react-icons/go";
+import { IoEyeOutline } from "react-icons/io5";
 
-// const Login = () => {
-//     useEffect(() => {
-//         document.title = 'REALESTATE/LOGIN '; 
-//     }, []); 
-//     const { signInUser } = useAuth();
-//     const { register, handleSubmit, formState: { errors } } = useForm();
+const Login = () => {
+    useEffect(() => {
+        document.title = 'REALESTATE/LOGIN '; 
+    }, []); 
+    const { signInUser } = useAuth();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     
-//     // navigation systems
-//     const navigate = useNavigate();
-//     const location = useLocation();
-//     const from = location?.state || "/";
-//     // State to manage password visibility
-//     const [showPassword, setShowPassword] = useState(false);
+    // navigation systems
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location?.state || "/";
+    // State to manage password visibility
+    const [showPassword, setShowPassword] = useState(false);
 
-//     // handle form submission
-//     const onSubmit = async (data) => {
-//         const { email, password } = data;
+    // handle form submission
+    const onSubmit = async (data) => {
+        const { email, password } = data;
 
-//         try {
-//             await signInUser(email, password);
-//             navigate(from);
-//         } catch (error) {
-//             // Display error message using toast
-//             toast.error('Invalid email or password');
-//         }
-//     };
+        try {
+            await signInUser(email, password);
+            navigate(from);
+        } catch (error) {
+            // Display error message using toast
+            toast.error('Invalid email or password');
+        }
+    };
 
-//     return (
-//         <>
-//             <ToastContainer />
-//             <div className='bg-base-200 pt-5 mt-40' data-aos="zoom-in">
-//                 <h1 className="text-5xl font-bold text-center ">Login now!</h1>
-//                 <div className="hero min-h-screen">
-//                     <div className="hero-content flex-col lg:flex-row-reverse">
-//                         <div className="card flex-shrink-0 w-full lg:w-[500px] pb-4 shadow-2xl bg-base-100">
-//                             <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-//                                 <div className="form-control">
-//                                     <label className="label">
-//                                         <span className="label-text">Email</span>
-//                                     </label>
-//                                     <input type="text" placeholder="email" className="input input-bordered"
-//                                     {...register("email", { required: true })}
-//                                     />
-//                                     {errors.email && <span className='text-red-500'>This field is required</span>}
-//                                 </div>
-//                                 <div className="form-control">
-//                                     <label className="label">
-//                                         <span className="label-text">Password</span>
-//                                     </label>
+    return (
+        <>
+            <ToastContainer />
+            <div className='bg-base-200 pt-5 mt-40' data-aos="zoom-in">
+                <h1 className="text-5xl font-bold text-center ">Login now!</h1>
+                <div className="hero min-h-screen">
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                        <div className="card flex-shrink-0 w-full lg:w-[500px] pb-4 shadow-2xl bg-base-100">
+                            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="text" placeholder="email" className="input input-bordered"
+                                    {...register("email", { required: true })}
+                                    />
+                                    {errors.email && <span className='text-red-500'>This field is required</span>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
                                     
-//                                         <input type={showPassword ? "text" : "password"} placeholder="password" className="input input-bordered pr-10"
-//                                             {...register("password", { required: true })}                                                      
-//                                         />
-//                                          <button
-//                                             type="button"
-//                                             className=" ml-96 -mt-6 transform -translate-y-1/2  text-gray-500 focus:outline-none"
-//                                             onClick={() => setShowPassword(!showPassword)}
-//                                         >
-//                                             {showPassword ? <IoEyeOutline className='h-10 w-6' /> : <GoEyeClosed className='h-10 w-6' />}
-//                                         </button>
+                                        <input type={showPassword ? "text" : "password"} placeholder="password" className="input input-bordered pr-10"
+                                            {...register("password", { required: true })}                                                      
+                                        />
+                                         <button
+                                            type="button"
+                                            className=" ml-96 -mt-6 transform -translate-y-1/2  text-gray-500 focus:outline-none"
+                                            onClick={() => setShowPassword(!showPassword)}
+                                        >
+                                            {showPassword ? <IoEyeOutline className='h-10 w-6' /> : <GoEyeClosed className='h-10 w-6' />}
+                                        </button>
                                        
                                 
-//                                     {errors.password && <span className='text-red-500'>This field is required</span>}
+                                    {errors.password && <span className='text-red-500'>This field is required</span>}
                                   
-//                                     <label className="label">
-//         <a
-//           onClick={() => document.getElementById("my_modal_2").showModal()}
-//           className="label-text-alt link link-hover"
-//         >
-//           Forgot password?
-//         </a>
-//       </label>
-//                                 </div>
-//                                 <div className="form-control mt-6 p-0">
-//                                     <button className="btn btn-neutral">Login</button>
-//                                 </div>
-//                                 <label className="label">
-//                                     New here? <Link to="/register" className="label-text-alt link link-hover">Create an account</Link>
-//                                 </label>
-//                             </form>
-//                             <SocialLogin />
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
+                                    <label className="label">
+        <a
+          onClick={() => document.getElementById("my_modal_2").showModal()}
+          className="label-text-alt link link-hover"
+        >
+          Forgot password?
+        </a>
+      </label>
+                                </div>
+                                <div className="form-control mt-6 p-0">
+                                    <button className="btn btn-neutral">Login</button>
+                                </div>
+                                <label className="label">
+                                    New here? <Link to="/register" className="label-text-alt link link-hover">Create an account</Link>
+                                </label>
+                            </form>
+                            <SocialLogin />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
-// export default Login;
+export default Login;
